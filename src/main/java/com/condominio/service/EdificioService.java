3d6e6f9c0cfe2cad47090bc8716422c6f3f66778
ISAAC12345-WEB <89948658+ISAAC12345-WEB@users.noreply.github.com>
2022-04-17 19:24:00 +0200

    Update EdificioService.java

--- src/main/java/com/condominio/service/EdificioService.java
+++ src/main/java/com/condominio/service/EdificioService.java
@@ -9,9 +9,4 @@ public interface EdificioService {
 
 	public abstract List<Edificio> listaEdifico();
 	public abstract Edificio insertaActualizaEdifico(Edificio obj);
-	/*public abstract List<Edifico> listaAlumnoPorDni(String dni);
-	public abstract Optional<Edifico> buscaPorId(int idAlumno);
-	public abstract List<Edifico> listaAlumnoPorDniDiferenteDelMismo(String dni, int idAlumno);
-	public abstract void eliminaPorId(int idAlumno);
-	public abstract List<Edifico> listaUsuarioPorNombreOCorreo(String nombre, String correo);*/
 }

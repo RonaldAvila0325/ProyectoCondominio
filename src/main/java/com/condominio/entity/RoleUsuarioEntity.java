0e2eb84fc5ead19164ee40f4932bc29a312ecf9a
RoSL <RoSL@DESKTOP-2MV7FRC>
2022-04-18 12:57:25 -0500

    First commit for the integrative project - Test

---
+++ src/main/java/com/condominio/entity/RoleUsuarioEntity.java
@@ -0,0 +1,50 @@
+package com.condominio.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RoleUsuarioEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator = "native")
+	private int idRol;
+	
+	@Column
+	private String descripcion;
+	
+	@Column String fechaRegistro;
+
+	
+	///// 
+	
+	public int getIdRol() {
+		return idRol;
+	}
+
+	public void setIdRol(int idRol) {
+		this.idRol = idRol;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(String fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	
+	
+	
+}
